93db0a288d9b418eb12c0742f49fc310bafe9a62
gouravpentaho <79900074+gouravpentaho@users.noreply.github.com>
2021-09-29 17:14:20 +0530

    Added working setup for Pentaho server with postgres

--- Script/pgsql.sh
+++ Script/pgsql.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-su postgres
-
-psql -c "\i /data/postgresql/create_quartz_postgresql.sql"
-psql -c "\i /data/postgresql/create_repository_postgresql.sql"
-psql -c "\i /data/postgresql/create_jcr_postgresql.sql"
+psql -U postgres -c "\i /data/create_quartz_postgresql.sql"
+psql -U postgres -c "\i /data/create_repository_postgresql.sql"
+psql -U postgres -c "\i /data/create_jcr_postgresql.sql"
