86cf853b76cb7624edeb654f61ddf0b87a0545e8
devuser <devuser@ddd3c72454.cs1cloud.internal>
2021-10-01 06:57:50 -0400

    folder restructuring and added working setups

--- winsetup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo "Hello World !!"
-curl http://build.pentaho.net/hosted/9.2.0.0/290/kettle-sdk-plugin-assembly-9.2.0.0-290.zip.sum -o kettle-sdk-plugin-assembly-9.2.0.0-290.zip.sum
-echo "Download completed"
